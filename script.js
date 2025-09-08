87751f8deb7388252439b559f504c8f9d9de2795
Rini Agrawal <161831644+riniagrawal252@users.noreply.github.com>
2025-09-08 14:13:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ console.log("Welcome to Spotify");
 // Initialize the Variables
 let songIndex = 0;
 let audioElement = new Audio('songs/1.mp3');
-let masterPlay = document.getElementById('masterPlay');
+let masterPlay = document.getElementByClassName('icons');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
 
